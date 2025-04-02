8065189b6e5778685f199ee390b9d4abc680c039
kmeye <kmeyersteffensen@gmail.com>
2025-04-02 12:00:42 +0200

    first commit

---
+++ src/main/java/com/example/madspildsprojekt/MadspildsProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.madspildsprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadspildsProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadspildsProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/madspildsprojekt/MadspildsProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madspildsprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadspildsProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
